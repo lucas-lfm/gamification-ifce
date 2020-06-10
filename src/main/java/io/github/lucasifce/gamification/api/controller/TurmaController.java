64bfed7a644c1273295db93d099c4751a6848d0c
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-06-10 12:06:14 -0300

    refatorando deletemapping de turma para deixar padronizado com boas práticas.

--- src/main/java/io/github/lucasifce/gamification/api/controller/TurmaController.java
+++ src/main/java/io/github/lucasifce/gamification/api/controller/TurmaController.java
@@ -1,8 +1,9 @@
 package io.github.lucasifce.gamification.api.controller;
 
-import io.github.lucasifce.gamification.api.dto.ProfessorTurmaRemoveListDTO;
-import io.github.lucasifce.gamification.api.dto.TrocaResponsavelTurmaDTO;
-import io.github.lucasifce.gamification.api.dto.TrocaStatusTurmaDTO;
+import io.github.lucasifce.gamification.api.dto.matriculaTurma.AlunoTurmaRemoveListDTO;
+import io.github.lucasifce.gamification.api.dto.professor.ProfessorTurmaRemoveListDTO;
+import io.github.lucasifce.gamification.api.dto.turma.TrocaResponsavelTurmaDTO;
+import io.github.lucasifce.gamification.api.dto.turma.TrocaStatusTurmaDTO;
 import io.github.lucasifce.gamification.api.dto.matriculaTurma.AlunoTurmaInsertListDTO;
 import io.github.lucasifce.gamification.api.dto.professor.ProfessorTurmaInsertListDTO;
 import io.github.lucasifce.gamification.api.dto.ranking.RankingListTurmaDTO;
@@ -37,10 +38,11 @@ public class TurmaController {
         turmaService.addNewListProfessor(dto);
     }
 
-    @DeleteMapping("/remover-professores")//corrigir para delete
+    @DeleteMapping("/remover-professores/{idTurma}")//corrigir para delete
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeListProfessor(@RequestBody @Valid ProfessorTurmaRemoveListDTO dto){
-        turmaService.removeListProfessor(dto);
+    public void removeListProfessor(@RequestBody @Valid ProfessorTurmaRemoveListDTO dto,
+                                    @PathVariable("idTurma") Long idTurma){
+        turmaService.removeListProfessor(dto, idTurma);
     }
 
     @PostMapping("/inserir-alunos")
@@ -49,10 +51,10 @@ public class TurmaController {
         turmaService.addNewListAluno(dto);
     }
 
-    @DeleteMapping("/remover-alunos")
+    @DeleteMapping("/remover-alunos/{idTurma}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void removeListAluno(@RequestBody @Valid AlunoTurmaInsertListDTO dto){
-        turmaService.removeListAluno(dto);
+    public void removeListAluno(@RequestBody @Valid AlunoTurmaRemoveListDTO dto, @PathVariable("idTurma") Long idTurma){
+        turmaService.removeListAluno(dto, idTurma);
     }
     
     @GetMapping("/ranking/{idTurma}")
@@ -60,10 +62,11 @@ public class TurmaController {
     	return rankingService.buscarRankingPorTurma(idTurma);
     }
 
-    @PatchMapping("/trocar-responsavel")
+    @PatchMapping("/trocar-responsavel/{idTurma}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateResponsavelTurma(@RequestBody @Valid TrocaResponsavelTurmaDTO dto) {
-        turmaService.updateProfessorResponsavel(dto);
+    public void updateResponsavelTurma(@RequestBody @Valid TrocaResponsavelTurmaDTO dto,
+                                       @PathVariable("idTurma") Long idTurma) {
+        turmaService.updateProfessorResponsavel(dto, idTurma);
     }
 
     @PatchMapping("/trocar-status/{id_turma}")
