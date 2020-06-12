c816a83a95f9f682b733e6dc865790b281f2a9ce
lucasifce <lucasifce>
2020-06-12 00:10:41 -0300

    Configurando pom.xml para deploy

--- src/main/java/io/github/lucasifce/gamification/GamificationIfceApplication.java
+++ src/main/java/io/github/lucasifce/gamification/GamificationIfceApplication.java
@@ -2,9 +2,10 @@ package io.github.lucasifce.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GamificationIfceApplication {
+public class GamificationIfceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamificationIfceApplication.class, args);
